d3e41083bc1d522d059b14c499c11bfd4bc2cf58
Amnshuman <75073445+Anshu-Man98@users.noreply.github.com>
2021-05-11 16:57:36 +0530

    Add files via upload

--- EmployeeDeactivation/Interface/IEmployeeDataOperations.cs
+++ EmployeeDeactivation/Interface/IEmployeeDataOperations.cs
@@ -4,12 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace EmployeeDeactivation.Interface
 {
     public interface IEmployeeDataOperations
     {
         List<Teams> RetrieveEmployeeData();
-        Task<bool> AddEmployeeData(string firstName, string lastName, string gId, string email, DateTime lastWorkingDate);
+        Task<bool> AddEmployeeData(string firstName, string lastName, string gId, string email, DateTime lastWorkingDate, string teamName, string sponsorName, string sponsorEmailId, string sponsorDepartment);
+        DeactivatedEmployeeDetails RetrieveEmployeeDataBasedOnGid(string gId);
     }
 
 }
