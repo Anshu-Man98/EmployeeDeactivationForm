f8c01bdd4236dda0b557f5dd5ab28581277d3d00
Amnshuman <75073445+Anshu-Man98@users.noreply.github.com>
2021-05-18 09:45:59 +0530

    Add files via upload

---
+++ EmployeeDeactivation/Interface/IEmployeeDataOperation.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeDeactivation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace EmployeeDeactivation.Interface
+{
+    public interface IEmployeeDataOperation
+    {
+        List<Teams> RetrieveEmployeeData();
+        Task<bool> AddEmployeeData(string firstName, string lastName, string gId, string email, DateTime lastWorkingDate, string teamName, string sponsorName, string sponsorEmailId, string sponsorDepartment);
+        DeactivatedEmployeeDetails RetrieveEmployeeDataBasedOnGid(string gId);
+        string GetReportingManagerEmailId(string teamName);
+    }
+}
---
+++ EmployeeDeactivation/Interface/IPdfDataOperation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeDeactivation.Interface
+{
+    public interface IPdfDataOperation
+    {
+        void SendPdfAsEmailAttachment(string memoryStream, string employeeName, string teamName);
+        byte[] FillPdfForm(string GId);
+        void SendRemainderEmail();
+    }
+}
